b219f7e6945fdea1327c0df8aabe0dee973f85e3
deborapoh <deborapoh@gmail.com>
2019-03-31 15:01:35 -0300

    início do back e comunicação com o banco

---
+++ back/APIRest/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+
+POSTGRES = {
+    'user': 'postgres',
+    'password': 'dpomgk13',
+    'db': 'tasklist',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+manager = APIManager(app, flask_sqlalchemy_db=db)
