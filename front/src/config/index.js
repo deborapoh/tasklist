39957e3cadf2c792b67089ef003096ced49cca37
deborapoh <deborapoh@gmail.com>
2019-03-31 11:04:38 -0300

    criação da logo e alguns ícones

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,12 +1,1 @@
-const Config = (() => {
-  let configs = null;
-
-  if (process.env.NODE_ENV === 'development') {
-    configs = require('./configDev');
-  } else {
-    configs = require('./configProd');
-  }
-  return configs;
-})();
-
-export default Config;
+export const ROOT_URL = 'http://localhost';
