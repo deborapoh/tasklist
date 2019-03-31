b9acc857b5bd24def4039764f728bf496bac0806
deborapoh <deborapoh@gmail.com>
2019-03-31 14:40:07 -0300

    page styles

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,1 +1,1 @@
-export const ROOT_URL = 'http://localhost';
+export const ROOT_URL = 'http://localhost:8080/api';
--- front/src/services/index.js
+++ front/src/services/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const Task = {
   async getTasks() {
-    //const response = await axios.get();
+    const response = await axios.get(`${ROOT_URL}/task`);
 
     return response.data.objects;
   },
